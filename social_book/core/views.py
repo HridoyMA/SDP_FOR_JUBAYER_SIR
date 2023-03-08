e4328b8b2bd829f81a83abf14edb13f284a90c35
Anisur Rahman Mahmud <19202103107@cse.bubt.edu.bd>
2023-03-08 22:58:35 +0600

    upload module

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -60,6 +60,21 @@ def profile(request, pk):
     }
     return render(request, 'profile.html', context)
 
+@login_required(login_url='signin')
+def upload(request):
+    if request.method=='POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user,image=image,caption=caption)
+        new_post.save()
+
+        return redirect('/')
+
+    else:
+        return redirect('/')
+
 def follow(request):
     if request.method == 'POST':
         follower = request.POST['follower']
