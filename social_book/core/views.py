edd3752d89859d9bf7beb3746dcf576e22de17fa
Hridoy <19202103103@cse.bubt.edu.bd>
2023-03-08 22:55:29 +0600

    h1

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Profile, Post, LikePost, FollowersCount
+from itertools import chain
+import random
 
 # Create your views here.
+@login_required(login_url='signin')
+def search(request):
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        username_object = User.objects.filter(username__icontains=username)
+
+        username_profile = []
+        username_profile_list = []
+
+        for users in username_object:
+            username_profile.append(users.id)
+        for ids in username_profile:
+            profile_lists = Profile.objects.filter(id_user=ids)
+            username_profile_list.append(profile_lists)
+
+        username_profile_list = list(chain(*username_profile_list))
+    return render(request, 'search.html', {'user_profile': user_profile, 'username_profile_list': username_profile_list})
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin') 
