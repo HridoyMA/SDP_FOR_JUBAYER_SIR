c3b7d38b8f5aa36ee9945bb592b03d2d0e8ab56d
Sanjana Ayshi <67552924+SanjanAyshi@users.noreply.github.com>
2023-03-08 23:14:49 +0600

    setting_com

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -81,6 +81,34 @@ def profile(request, pk):
     return render(request, 'profile.html', context)
 
 @login_required(login_url='signin')
+def setting(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location  = request.POST['location']
+            
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location  = request.POST['location']
+            
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        return redirect('settings')
+
+    return render(request, 'setting.html',{'user_profile': user_profile})
+
+@login_required(login_url='signin')
 def upload(request):
     if request.method=='POST':
         user = request.user.username
