0180f0a19fcccf4e04280dc51290d575f2b62b21
Sajid Rahman <127319733+sajidrahman700@users.noreply.github.com>
2023-03-08 23:12:25 +0600

    sign in korsi

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -91,6 +91,20 @@ def follow(request):
     else:
         return redirect('/')
 
+def signin(request):
+    
+    if request.method =='POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,'Credentials Invalid')
+            return redirect('signin')
+    return render(request,'signin.html')
+
 @login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
