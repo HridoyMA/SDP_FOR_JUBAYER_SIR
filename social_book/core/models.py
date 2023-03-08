1ed6ba93ae6e67c28474385bfe2f77f554815477
Hridoy <19202103103@cse.bubt.edu.bd>
2023-03-08 23:27:56 +0600

    new2

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -5,3 +5,14 @@ from datetime import datetime
 
 User = get_user_model()
 # Create your models here.
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key = True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def _str_(self):
+        return self.user
